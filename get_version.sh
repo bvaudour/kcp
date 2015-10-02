7e4929ce5419f9aee2353950e3b4b11d31cdbac7
bvaudour <benjamin.vaudour@gmail.com>
2015-10-02 19:24:43 +0200

    update id_ID + modify get_version script

--- get_version.sh
+++ get_version.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-cat src/sysutil/util.go | grep "VERSION " | grep -v "D_VERSION" | sed 's/.*"\(.*\)".*/\1/'
+#cat src/sysutil/util.go | grep "VERSION " | grep -v "D_VERSION" | sed 's/.*"\(.*\)".*/\1/'
+sed -nre 's/.*VERSION.*"(.*)".*/\1/p' src/sysutil/util.go
