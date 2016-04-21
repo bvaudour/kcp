194b4329e1a59f317a8ef3bc6c007c91c0f5dbfb
bvaudour <benjamin.vaudour@gmail.com>
2016-04-21 17:39:39 +0200

    kcp 0.85.1 - Update locales + build optimizations

--- src/sysutil/util.go
+++ src/sysutil/util.go
@@ -14,7 +14,7 @@ const (
 	DEFAULT_YES    = "[Y/n]"
 	DEFAULT_NO     = "[y/N]"
 	AUTHOR         = "B. VAUDOUR"
-	VERSION        = "0.85"
+	VERSION        = "0.85.1"
 	KCP_LOCK       = "kcp.lock"
 	KCP_DB         = ".kcp.json"
 	LOCALE_DIR     = "/usr/share/locale"
