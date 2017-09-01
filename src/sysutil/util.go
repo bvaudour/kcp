1908d312cc58be05a6d24138a9296287e49073d0
bvaudour <benjamin.vaudour@gmx.fr>
2017-09-01 08:18:39 +0200

    kcp - 0.90.1 update i18n

--- src/sysutil/util.go
+++ src/sysutil/util.go
@@ -15,7 +15,7 @@ const (
 	DEFAULT_YES    = "[Y/n]"
 	DEFAULT_NO     = "[y/N]"
 	AUTHOR         = "B. VAUDOUR"
-	VERSION        = "0.90.0"
+	VERSION        = "0.90.1"
 	KCP_LOCK       = "kcp.lock"
 	KCP_DB         = ".kcp.json"
 	LOCALE_DIR     = "/usr/share/locale"
